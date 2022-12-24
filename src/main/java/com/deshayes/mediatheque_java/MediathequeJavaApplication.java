45e42c39ebd64c83c4f2f7b7eac202c56d818751
Dayann Deshayes <dayanndeshayes@gmail.com>
2022-12-24 11:49:20 +0100

    first commit

---
+++ src/main/java/com/deshayes/mediatheque_java/MediathequeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.deshayes.mediatheque_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediathequeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediathequeJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deshayes/mediatheque_java/MediathequeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deshayes.mediatheque_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediathequeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
